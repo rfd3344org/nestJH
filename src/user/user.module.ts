b8e21bea36f7952850854c9bf23bfaa49204868c
ericliS <eric.li@sunixvision.com.au>
2021-09-11 22:00:48 +1000

    no message

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import UserService from './user.service';
 import { User, UserSchema } from './user.schema';
-import UserController from './user.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UserService],
-  controllers: [UserController],
   exports: [UserService],
 })
 export default class UserModule {}
