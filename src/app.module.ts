b67f634c2a09a91b85b1cf9b1f5d010e7357383c
eric <rfd3344w@gmail.com>
2025-02-17 11:48:43 +1100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AppController } from './app.controller';
 
 const formatMongoURL = () => {
   const { MONGO_DB_NAME, MONGO_USER, MONGO_PASSWORD } = process.env;
-  return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.giwpq.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+  return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.lph5oow.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
 };
 
 @Module({
